a8cdeef82acb38c6c386e3ef550d5ee31b476608
Mcaliston95 <mcalistonlewis@gmail.com>
2024-12-03 16:39:58 -0500

    furniture Assgn

---
+++ Furniture.py
@@ -0,0 +1,14 @@
+itemname = "TV Stand"
+retailprice = 325.00
+wholesaleprice = 200.00
+
+profit = retailprice - wholesaleprice
+salesPrice = retailprice - (retailprice * 0.25)
+salesprofit = salesPrice - wholesaleprice
+
+print("Item Name:",itemname)
+print("retailprice",retailprice )
+print("wholesaleprice",wholesaleprice)
+print("profit",profit)
+print("sales",salesPrice)
+print("sales", salesprofit)
